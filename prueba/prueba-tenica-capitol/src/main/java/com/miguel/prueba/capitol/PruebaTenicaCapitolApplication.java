63c43de3b99609e111cadde6e365b73f5bf52227
Miguel <miguel.luque.97@gmail.com>
2021-12-24 17:24:37 +0100

    Initial project architecture & dependencies

---
+++ prueba/prueba-tenica-capitol/src/main/java/com/miguel/prueba/capitol/PruebaTenicaCapitolApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.prueba.capitol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTenicaCapitolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTenicaCapitolApplication.class, args);
+	}
+
+}
---
+++ prueba/prueba-tenica-capitol/src/test/java/com/miguel/prueba/capitol/PruebaTenicaCapitolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.prueba.capitol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTenicaCapitolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
